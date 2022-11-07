6473161099f8d4f51c58fc4f4654915700274994
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-11-07 18:12:45 -0500

    Create design0.m

---
+++ design0.m
@@ -0,0 +1,29 @@
+%% Title
+
+%% Double Percent for Section Break
+%{
+multiline
+comments
+when
+necessary
+
+to open as live script, right click tab and select open as live script
+- i plan to use comments for inline code notes (i played around with
+converting from live script to .m file with fancy markdown and it didn't
+work as well as expected
+- we will be submitting raw code anyway so editing .m files (and converting
+to .mlx files when necessary) would be more ideal
+%}
+
+% Since this is .m file, outputs to command window
+a = [1 2 3];
+a
+
+% Plots not to command window (hexagram best marker)
+plot(4, 4, "hexagram")
+
+%% Questions
+%{
+- can we still assume that the distance between the diaphragms is 400 despite the double diaphragm on the left/right end
+
+%}
