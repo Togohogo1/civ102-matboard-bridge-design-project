d4864671361d6851e9742acef088b482f302fcf1
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-11-07 20:53:09 -0500

    Update design0.m

--- design0.m
+++ design0.m
@@ -16,14 +16,18 @@ to .mlx files when necessary) would be more ideal
 %}
 
 % Since this is .m file, outputs to command window
-a = [1 2 3];
+a = [3 3 3 2 1 0 -1 -2 -2 -2];
 a
 
 % Plots not to command window (hexagram best marker)
-plot(4, 4, "hexagram")
+figure
+plot(a, "-hexagram")
+
+figure
+plot(cumsum(a), "-o")
 
 %% Questions
 %{
 - can we still assume that the distance between the diaphragms is 400 despite the double diaphragm on the left/right end
-
+- Table 30.2 ea 7, is the vertical direction not supported?
 %}
