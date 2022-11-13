abeca640e87f43d008a2d3f3cb936ec8170b4a6c
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-11-13 14:59:06 -0500

    refactor axis title, grid lines, flip axis

--- design0.m
+++ design0.m
@@ -30,7 +30,7 @@ plot(cumsum(a), "-o")
 close all;
 clear;
 
-x = 115;
+x = 0;
 
 y_left = (720-x)/3;
 y_right = (x+480)/3;
@@ -57,24 +57,28 @@ x_moment = [0, x+52, x+52+176, x+392, x+568, x+732, x+908, 1200];
 
 for i = 2:7
     x_val = (x_shear((i-1)*2+1)) - x_shear((i-1)*2-1);
-    moment(i) = moment(i-1) - (shear((i-1)*2 + 1) * x_val);
+    moment(i) = moment(i-1) + (shear((i-1)*2 + 1) * x_val);
 end
 
 moment;
 
 figure
 plot(x_shear, shear)
+grid on
+grid minor
 yline(0)
-xlabel("X Values(mm)")
-ylabel("Shear Force(N)")
-title("SFD")
+xlabel("Distance Along Bridge (mm)")
+ylabel("Shear Force (N)")
+title("Shear Force Diagram")
 
 figure
 plot(x_moment, moment)
-yline(0)
-xlabel("X Values(mm)")
-ylabel("BendingMoment(N.mm)")
-title("BMD")
+grid on
+grid minor
+set(gca, 'YDir', 'reverse')  % Flip y-axis
+xlabel("Distance Along Bridge (mm)")
+ylabel("Bending Moment (Nmm)")
+title("Bending Moment Diagram")
 
 %% Questions
 %{
