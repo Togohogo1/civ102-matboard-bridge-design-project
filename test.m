cc793a7a5fe451660e29e42493561c13de059e75
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-11-21 22:44:12 -0500

    damn file tracking with matlab live share hard

--- test.m
+++ test.m
@@ -7,26 +7,26 @@ x = 0;
 L = 1200;  % Length of bridge
 n = 1200;  % Discretize into 1 mm seg.
 P = 400;  % Total weight of train [N]
-x_increments = linspace(0, L, n+1);
+lhs_dist = linspace(0, L, n+1);
 x_shear_start = (720-x)/3;  % Starting shear at the pin/roller
 
 % One offset because MATLAB indexes with 1
 % Another offset to shift cumsum left 1
-x_shear_locations = [x+52, x+228, x+392, x+568, x+732, x+908];
+x_shear_locations = [x+52, x+228, x+392, x+568, x+732, x+908] + 2;
 x_train_loads = [-P/6, -P/6, -P/6, -P/6, -P/6, -P/6];
 
 x_shear = zeros(1, L+1);
-%x_shear(1) = x_shear_start;  % Starting shear at the pin/roller
-x_shear(x_shear_locations+1) = x_train_loads;
-x_shear = cumsum([x_shear, 0]);  % Final shear force diagram
+x_shear(1) = x_shear_start;  % Starting shear at the pin/roller
+x_shear(x_shear_locations) = x_train_loads;
+x_shear = cumsum(x_shear);  % Final shear force diagram
 
 x_moment = cumsum(x_shear) - x_shear_start;
 
+% Getting x_moment at 0mm -> query x_moment(1+1)
+% Getting x_moment at 1mm -> query x_moment(1+1)
 
-plot(x_shear)
+
+
+plot(lhs_dist, x_shear)
 figure
-plot(x_moment)
-
-x_moment(52)
-
-
+plot(lhs_dist, x_moment)
